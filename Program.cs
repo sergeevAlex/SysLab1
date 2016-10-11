8d1ab40e1c489368b94a6c34eb73313e73066ae7
sergeevAlex <alexsergeev@i.ua>
2016-10-11 20:36:13 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace SysLab
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+			//System.Diagnostics.Stopwatch sw2 = new System.Diagnostics.Stopwatch();
+			Random rd = new Random();
+			const int N = 10000;
+			int[] vector = new int[N];
+			int[,] matrix = new int[N,N];
+			WorkArray.rMatrix(matrix);
+			for (int i = 0; i < N; i++)
+			{
+				vector[i] = rd.Next(1,5);
+			}
+			Console.WriteLine("Матрица");
+			//WorkArray.printMatrix(matrix);
+			Console.WriteLine("Вектор");
+			//WorkArray.printArr(vector);
+			Console.WriteLine("Матрица на вектор:");
+			sw.Start();
+			//WorkArray.printArr(WorkArray.multiplyMV(vector,matrix));
+			WorkArray.multiplyMV(vector,matrix);
+			sw.Stop();
+			Console.WriteLine("Время умножения матрицы на вектор:" + sw.ElapsedMilliseconds);
+			Console.WriteLine("Вектор на матрицу");
+			sw.Start();
+			WorkArray.multiplyVM(vector, matrix);
+			sw.Stop();
+			Console.WriteLine("Время умножения вектора на матрицу:" + sw.ElapsedMilliseconds);
+			//WorkArray.printArr(WorkArray.multiplyVM(vector,matrix));
+		}
+	}
+}
---
+++ WorkArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace SysLab
+{
+	public class WorkArray
+	{
+		public WorkArray()
+		{
+		}
+		//Заполнение матрицы
+		public static int[,] rMatrix(int[,] array)
+		{
+			Random rd1 = new Random();
+			for (int i = 0; i < array.GetLength(0); i++)
+			{
+				for (int j = 0; j < array.GetLength(0); j++)
+				{
+					array[i,j] = rd1.Next(1, 5);	
+				}
+			}
+			return array;
+		}
+		//Печать
+		public static void printArr(int[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine(array[i]);
+			}
+		}
+		public static void printMatrix(int[,] array)
+		{
+			for (int i = 0; i < array.GetLength(0); i++)
+			{
+				for (int j = 0; j < array.GetLength(0); j++)
+				{
+					Console.Write(array[i,j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+		//Умножение матрицы на вектор
+		public static int[] multiplyMV(int[] array1, int[,] array2)
+		{
+			int[] result = new int[array1.Length];
+			for (int i = 0; i < array1.GetLength(0); i++)
+			{
+				result[i] = 0;
+				for (int j = 0; j < array1.GetLength(0); j++)
+				{
+						result[i] += array2[i, j] * array1[j];
+				}
+			}
+			//printArr(result);
+			return result;
+		}
+		public static int[] multiplyVM(int[] array1, int[,] array2)
+		{
+			int[] result = new int[array1.Length];
+			for (int i = 0; i < array1.GetLength(0); i++)
+			{
+				result[i] = 0;
+				for (int j = 0; j < array1.GetLength(0); j++)
+				{
+					result[i] += array1[j]*array2[j,i];
+				}
+			}
+			return result;
+		}
+
+	}
+}
